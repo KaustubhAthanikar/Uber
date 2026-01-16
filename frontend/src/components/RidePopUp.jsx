38f8d1136aed24a1914c775e947bb85539d530ae
Kaustubh Athanikar <athanikarkaustubh@gmail.com>
2026-01-16 21:28:04 +0530

    Frontend done

---
+++ frontend/src/components/RidePopUp.jsx
@@ -0,0 +1,63 @@
+import React, { useRef } from 'react'
+
+const RidePopUp = (props) => {
+    return (
+        <div>
+            <h5 className='p-1 text-center absolute top-0  w-[93%]'
+                onClick={() => {
+                    props.setRidePopUpPanel(false);
+                }}><i className="text-3xl text-gray-300 ri-arrow-down-wide-fill"></i></h5>
+            <h3 className='text-2xl font-semibold mb-3'>New ride available</h3>
+
+            <div className='flex items-center justify-between mt-4 bg-yellow-400 p-3 rounded-lg'>
+                <div className='flex items-center gap-3 '>
+                    <img className='h-12 w-12 rounded-full object-cover' src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fHww" alt="" />
+                    <h2 className='text-xl font-medium'>Harsh Patel</h2>
+
+                </div>
+                <h5 className='text-lg font-semibold'>2.2Km</h5>
+            </div>
+
+            <div className='flex flex-col gap-2 justify-between items-center'>
+                <div className='w-full mt-5'>
+                    <div className='flex items-center gap-3 p-3 border-b-2 border-gray-200'>
+                        <h4><i className="text-lg ri-map-pin-line"></i></h4>
+                        <div>
+                            <h3 className='text-lg font-medium'>562/11-A</h3>
+                            <p className='text-gray-600 text-sm -mt-1'> Kalpavruksha Society Jawalkar Nagar Pimple Gurav,Pune</p>
+                        </div>
+                    </div>
+                    <div className='flex items-center gap-3 p-3 border-b-2 border-gray-200'>
+                        <h4><i className="text-lg ri-map-pin-line"></i></h4>
+                        <div>
+                            <h3 className='text-lg font-medium'>562/11-A</h3>
+                            <p className='text-gray-600 text-sm -mt-1'> Kalpavruksha Society Jawalkar Nagar Pimple Gurav,Pune</p>
+                        </div>
+                    </div>
+                    <div className='flex items-center gap-3 p-3 '>
+                        <h4 className='text-xl'><i className="ri-money-rupee-circle-line"></i></h4>
+                        <div>
+                            <h3 className='text-lg font-medium'>₹193.20</h3>
+                            <p className='text-gray-600 text-sm -mt-1'>Cash</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div className=' mt-5 w-full'>
+                    <button onClick={() => {
+                    props.setConfirmRidePopUpPanel(true)
+                }
+                } className='w-full bg-green-500 text-white font-semibold p-3 px-10 rounded-lg'>Accept</button>
+
+                <button onClick={() => {
+                   props.setRidePopUpPanel(false)
+                }
+                } className='w-full mt-2 bg-gray-400 text-white font-semibold p-3 px-10 rounded-lg'>Ignore</button>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default RidePopUp
--- frontend/src/pages/CaptainHome.jsx
+++ frontend/src/pages/CaptainHome.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { Link } from 'react-router-dom'
+import CaptainDetails from '../components/CaptainDetails'
+import RidePopUp from '../components/RidePopUp'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import ConfirmRidePopUp from '../components/ConfirmRidePopUp'
 
 const CaptainHome = () => {
+   const [ridePopUpPanel, setRidePopUpPanel] = React.useState(true);
+    const ridePopUpPanelRef = useRef(null);
+
+    const [confirmRidePopUpPanel, setConfirmRidePopUpPanel] = React.useState(false);
+    const confirmRidePopUpPanelRef = useRef(null);
+
+    useGSAP(() => {
+        if (ridePopUpPanel) {
+            gsap.to(ridePopUpPanelRef.current, {
+                transform: 'translateY(0%)',
+                duration: 0.4,
+                ease: 'power2.out'
+            });
+        } else {
+            gsap.to(ridePopUpPanelRef.current, {
+                transform: 'translateY(100%)',
+                duration: 0.4,
+                ease: 'power2.in'
+            });
+        }
+    }, [ridePopUpPanel]);
+    useGSAP(() => {
+        if (confirmRidePopUpPanel) {
+            gsap.to(confirmRidePopUpPanelRef.current, {
+                transform: 'translateY(0%)',
+                duration: 0.4,
+                ease: 'power2.out'
+            });
+        } else {
+            gsap.to(confirmRidePopUpPanelRef.current, {
+                transform: 'translateY(100%)',
+                duration: 0.4,
+                ease: 'power2.in'
+            });
+        }
+    }, [confirmRidePopUpPanel]);
   return (
-    <div>
+    <div className='h-screen'>
+      <div className='fixed p-3 top-0 flex items-center justify-between w-screen'>
+        <img className='w-16' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
+        <Link to='/captain-home' className=' h-10 w-10 bg-white flex items-center justify-center rounded-full'>
+          <i className="ri-logout-box-r-line"></i>
+        </Link>
+      </div>
+      <div className='h-3/5'>
+        <img
+          className="h-full w-full object-cover"
+          src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif"
+          alt="map"
+        />
+      </div>
+      <div className='h-2/5 p-6'>
+        <CaptainDetails />
+      </div>
       
+      <div ref={ridePopUpPanelRef} className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12'>
+        <RidePopUp setRidePopUpPanel={setRidePopUpPanel} setConfirmRidePopUpPanel={setConfirmRidePopUpPanel} />
+      </div>
+
+      <div ref={confirmRidePopUpPanelRef} className='fixed w-full h-screen z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12'>
+        <ConfirmRidePopUp setConfirmRidePopUpPanel={setConfirmRidePopUpPanel} setRidePopUpPanel={setRidePopUpPanel} />
+      </div>
+
     </div>
   )
 }
